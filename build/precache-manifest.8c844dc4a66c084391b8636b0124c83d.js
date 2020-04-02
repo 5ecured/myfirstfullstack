bae1fb5fb3180f4d39b95bf3a9cc96e2bc801e71
Edward Ottorino <edward.ottorino@gmail.com>
2020-04-02 12:30:20 +1100

    'fs-commit'

---
+++ build/precache-manifest.8c844dc4a66c084391b8636b0124c83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af930074e22ff8db2fef31084731f4f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a06522c0a6b36804b063",
+    "url": "/static/css/main.48a7c636.chunk.css"
+  },
+  {
+    "revision": "70e7ce330f0b45c41aae",
+    "url": "/static/js/2.383c1124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.383c1124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06522c0a6b36804b063",
+    "url": "/static/js/main.5d69b803.chunk.js"
+  },
+  {
+    "revision": "3a1a90a1317851a1f520",
+    "url": "/static/js/runtime-main.9c312146.js"
+  }
+]);
